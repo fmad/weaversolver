99ff1f83636bd9d5c49502b21422df5d9505ba5a
Fernando Madruga <1113955+fmad@users.noreply.github.com>
2022-11-05 21:47:03 +0000

    Removed option to specify Dictionary
    This can be safely inferred from the word lengths

--- solver.ps1
+++ solver.ps1
@@ -6,11 +6,7 @@
     [string]$To,
 
     [Parameter(Mandatory=$false)]
-    [switch]$Silent,
-    
-    [ValidateRange(4,5)]
-    [Parameter(Mandatory=$false)]
-    [int]$Dict = 4
+    [switch]$Silent
 )
 $From = $From.ToLower()
 $To   = $To.ToLower()
@@ -18,6 +14,7 @@ if ( $From.Length -ne $To.Length -or $From.Length -lt 4 -or $From.Length -gt 5) 
     Write-Error "From and To need to be either 4 or 5 characters long words, and both need to be the same length."
     return
 }
+$Dict = $From.Length
 # Load Wordlists
 Set-Location -LiteralPath $PSScriptRoot
 $in    = Get-Content "english-$($Dict)letters.txt"
