ab2a217c019ded47d1c8cd1f78f69bc123bbbf85
Fernando Madruga <1113955+fmad@users.noreply.github.com>
2022-11-05 21:40:49 +0000

    Correcting typo

--- solver.ps1
+++ solver.ps1
@@ -15,7 +15,7 @@
 $From = $From.ToLower()
 $To   = $To.ToLower()
 if ( $From.Length -ne $To.Length -or $From.Length -lt 4 -or $From.Length -gt 5) {
-    Write-Error "From and To need to be either 4 or 5 characters long words, and both need to be the same length.@
+    Write-Error "From and To need to be either 4 or 5 characters long words, and both need to be the same length."
     return
 }
 # Load Wordlists
